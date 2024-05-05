0a85a86db79a6cc5dee48f56aea1bac6a16785c5
Otabek <otabekrahmonovtest@gmail.com>
2024-05-05 16:58:54 +0500

    feat: Admin base template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,134 @@
-Admin dashboard
+@extends('admin.layouts.main')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-sub-header">
+                    <h3 class="page-title">Welcome Admin!</h3>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active">Admin</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-xl-3 col-sm-6 col-12 d-flex">
+            <div class="card bg-comman w-100">
+                <div class="card-body">
+                    <div class="db-widgets d-flex justify-content-between align-items-center">
+                        <div class="db-info">
+                            <h6>Students</h6>
+                            <h3>50055</h3>
+                        </div>
+                        <div class="db-icon">
+                            <img src="assets/img/icons/dash-icon-01.svg" alt="Dashboard Icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12 d-flex">
+            <div class="card bg-comman w-100">
+                <div class="card-body">
+                    <div class="db-widgets d-flex justify-content-between align-items-center">
+                        <div class="db-info">
+                            <h6>Awards</h6>
+                            <h3>50+</h3>
+                        </div>
+                        <div class="db-icon">
+                            <img src="assets/img/icons/dash-icon-02.svg" alt="Dashboard Icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12 d-flex">
+            <div class="card bg-comman w-100">
+                <div class="card-body">
+                    <div class="db-widgets d-flex justify-content-between align-items-center">
+                        <div class="db-info">
+                            <h6>Department</h6>
+                            <h3>30+</h3>
+                        </div>
+                        <div class="db-icon">
+                            <img src="assets/img/icons/dash-icon-03.svg" alt="Dashboard Icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12 d-flex">
+            <div class="card bg-comman w-100">
+                <div class="card-body">
+                    <div class="db-widgets d-flex justify-content-between align-items-center">
+                        <div class="db-info">
+                            <h6>Revenue</h6>
+                            <h3>$505</h3>
+                        </div>
+                        <div class="db-icon">
+                            <img src="assets/img/icons/dash-icon-04.svg" alt="Dashboard Icon">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-lg-6">
+
+            <div class="card card-chart">
+                <div class="card-header">
+                    <div class="row align-items-center">
+                        <div class="col-6">
+                            <h5 class="card-title">Overview</h5>
+                        </div>
+                        <div class="col-6">
+                            <ul class="chart-list-out">
+                                <li><span class="circle-blue"></span>Teacher</li>
+                                <li><span class="circle-green"></span>Student</li>
+                                <li class="star-menus"><a href="javascript:;"><i class="fas fa-ellipsis-v"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div id="apexcharts-area"></div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-12 col-lg-6">
+
+            <div class="card card-chart">
+                <div class="card-header">
+                    <div class="row align-items-center">
+                        <div class="col-6">
+                            <h5 class="card-title">Number of Students</h5>
+                        </div>
+                        <div class="col-6">
+                            <ul class="chart-list-out">
+                                <li><span class="circle-blue"></span>Girls</li>
+                                <li><span class="circle-green"></span>Boys</li>
+                                <li class="star-menus"><a href="javascript:;"><i class="fas fa-ellipsis-v"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div id="bar"></div>
+                </div>
+            </div>
+
+        </div>
+    </div> 
+@endsection
---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,74 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">
+                    <span>Main Menu</span>
+                </li>
+                <li class="submenu active">
+                    <a href="#"><i class="feather-grid"></i> <span> Dashboard</span>  </a> 
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fas fa-graduation-cap"></i> <span> Students</span> <span
+                            class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Student List</a></li>
+                        <li><a href="#">Student View</a></li>
+                        <li><a href="#">Student Add</a></li>
+                        <li><a href="#">Student Edit</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fas fa-chalkboard-teacher"></i> <span> Teachers</span> <span
+                            class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Teacher List</a></li>
+                        <li><a href="#">Teacher View</a></li>
+                        <li><a href="#">Teacher Add</a></li>
+                        <li><a href="#">Teacher Edit</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fas fa-building"></i> <span> Departments</span> <span
+                            class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Department List</a></li>
+                        <li><a href="#">Department Add</a></li>
+                        <li><a href="#">Department Edit</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fas fa-book-reader"></i> <span>Courses</span> <span
+                            class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Course List</a></li>
+                        <li><a href="#">Course Add</a></li>
+                        <li><a href="#">Course Edit</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fas fa-book-reader"></i> <span> Rooms</span> <span
+                            class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Room List</a></li>
+                        <li><a href="#">Room Add</a></li>
+                        <li><a href="#">Room Edit</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fas fa-clipboard"></i> <span> Invoices</span> <span
+                            class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Invoices List</a></li>
+                        <li><a href="#">Invoices Grid</a></li>
+                        <li><a href="#">Add Invoices</a></li>
+                        <li><a href="#">Edit Invoices</a></li>
+                        <li><a href="#">Invoices Details</a></li>
+                        <li><a href="#">Invoices Settings</a></li>
+                    </ul>
+                </li> 
+                 
+            </ul>
+        </div>
+    </div>
+</div>
